f418b6c7d6d0621849cb08f93b87ca727ab4b7a0
Justin Nguyen <Justin.Nguyen77@yahoo.com>
2025-09-04 13:10:43 -0500

    created imports for everything I'll need for welcome page

--- src/app/(WelcomeView)/page.tsx
+++ src/app/(WelcomeView)/page.tsx
@@ -1,3 +1,8 @@
+import * as React from 'react'
+import { Button } from '@/components/ui/button'
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import { Shield, TrendingUp, Clock, Square } from "lucide-react"
+
 export default function Home() {
   return (
     <div>
