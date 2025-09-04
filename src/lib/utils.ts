4971a09355539aef9eb2845de72dbd87321d349c
Justin Nguyen <Justin.Nguyen77@yahoo.com>
2025-09-04 11:54:37 -0500

    Renamed placeholder folder to Welcome for welcome page, added a descriptive comment for the cn funtion in utils.ts under the lib folder, Created a style folder to hold css folders

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+//this function merges css classes and removes duplicates, use when you don't want long className strings
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
