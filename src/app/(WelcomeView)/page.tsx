70114de6d0767f6713ce1d42b58e4eba7ae067f9
Justin Nguyen <Justin.Nguyen77@yahoo.com>
2025-09-08 13:05:46 -0500

    Fixed merge conflicts in recently edited files.

--- src/app/(WelcomeView)/page.tsx
+++ src/app/(WelcomeView)/page.tsx
@@ -1,13 +1,7 @@
 /* 
-<<<<<<< HEAD
 Author: Justin Nguyen
 Last Updated: 09/08/2025
 Purpose: Landing page for ClutchCall, explaining what it is and its features
-=======
-    Author: Justin Nguyen
-    Last Updated: 09/08/2025
-    Purpose: Landing page for ClutchCall, explaining what it is and its features
->>>>>>> b08edd8cdd542ff6c1ece32354d53aab3ccbd453
 */
 
 import * as React from 'react'
