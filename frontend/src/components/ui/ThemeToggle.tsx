08ad480775d558dd845f0ebfeab66bdb05770926
Justin <justin.nguyen77@yahoo.com>
2025-09-20 18:38:13 -0500

    Hovering over buttons and theme toggle makes the cursor a pointer

--- frontend/src/components/ui/ThemeToggle.tsx
+++ frontend/src/components/ui/ThemeToggle.tsx
@@ -31,7 +31,7 @@ export const ThemeToggle = () => {
     }
 
     return (
-        <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-4 right-4 z-50 p-2 rounded-full transition-colors duration-300",
+        <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-4 right-4 z-50 p-2 rounded-full transition-colors duration-300 cursor-pointer",
             "focus:outline-hidden"
         )}> 
             {isDarkMode ? ( 
