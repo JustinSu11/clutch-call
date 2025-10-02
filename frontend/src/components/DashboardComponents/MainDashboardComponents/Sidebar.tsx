1dc9b294197fd56457c7d39af71254e1417f075e
Justin <justin.nguyen77@yahoo.com>
2025-10-01 20:01:26 -0500

    selected nav items are more obvious now

--- frontend/src/components/DashboardComponents/MainDashboardComponents/Sidebar.tsx
+++ frontend/src/components/DashboardComponents/MainDashboardComponents/Sidebar.tsx
@@ -7,9 +7,24 @@
 
     Description: This file has the code for the sidebar component in the dashboard.
 */
-
+"use client"
 import { ThemeToggle } from "@/components/ui/ThemeToggle"
-import { Home, Target } from "lucide-react"
+import { Home, ChartScatter } from "lucide-react"
+import { usePathname } from "next/navigation"
+import Link from "next/link"
+
+const navItems = [
+    {
+        title: 'Home',
+        href: '/dashboard',
+        icon: Home,
+    },
+    {
+        title: 'Predictions',
+        href: '/predictions',
+        icon: ChartScatter,
+    }
+]
 
 export default function Sidebar() {
     /*
@@ -20,6 +35,11 @@ export default function Sidebar() {
         Returns:
         A JSX element representing the sidebar component.
     */
+
+    //This is used in the className for the links that get generated for each nav item. if currentTitle (title of the nav item whose href matches the path in the url) matches with the title of the nav item then that nav item will have classes applied to it that make it look like its selected
+    const pathname = usePathname()
+    const currentTitle = navItems.find(item => pathname?.startsWith(item.href))?.title
+
     return (
         <div>
             {/* <aside> defines a secondary component like sidebars. It's considered secondary content for the page*/}
@@ -29,15 +49,15 @@ export default function Sidebar() {
                     <h1 className="flex justify-center text-4xl font-bold text-text-primary mb-8">ClutchCall</h1>
                     {/* <nav> groups links together */}
                     <nav className="flex flex-col gap-2">
-                        <a className="flex items-center gap-3 px-4 py-3 rounded-lg bg-secondary text-primary font-medium" href="/dashboard">
-                            <Home />
-                            {/* The text label for the tab */}
-                            <span>Home</span>
-                        </a>
-                        <a className="flex items-center gap-3 px-4 py-3 rounded-lg bg-secondary text-primary font-medium" href="/predictions">
-                            <Target />
-                            <span>Predictions</span>
-                        </a>
+                        {
+                            navItems.map((item) => (
+                                <Link className={`flex items-center gap-3 px-4 py-3 rounded-lg text-text-primary font-medium hover:text-red-600 ${item.title === currentTitle ? "bg-secondary text-primary" : "text-text-primary"}`} href={item.href} id={item.title} key={item.title}>
+                                    <item.icon />
+                                    {/* The text label for the tab */}
+                                    <span>{item.title}</span>
+                                </Link>
+                            ))
+                        }
                     </nav>
                 </div>
                 <div className="mx-auto">
