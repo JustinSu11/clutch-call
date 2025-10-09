2b797a75d6970205cf168076d216c06c99cfa865
CJQuintero61 <cjquintero61@gmail.com>
2025-10-09 18:05:00 -0500

    remove leader stuff

--- frontend/src/utils/data_class.tsx
+++ frontend/src/utils/data_class.tsx
@@ -10,13 +10,4 @@ export type UpcomingGame = {
     homeTeam: string;
     awayTeam: string;
     gameDate: string;                       // YYYY-MM-DD
-    leaders?: [];                           // array of leaders, optional attr
 };
-
-export type Leader = {
-    title: string;              // e.g., "Passing Yards"
-    playerID: number | string;        // e.g., 2543484
-    playerName: string;         // e.g., "P. Mahomes"
-    value: number | string;     // e.g., 312 or "312"
-    playerHeadshot?: string;   // URL to player's headshot image, optional
-}
